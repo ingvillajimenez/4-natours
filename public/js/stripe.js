2a7db8f19dc3f5cd2ee8f576b16cd81daabd5a62
Juan Villa <ing.villa.jimenez@gmail.com>
2024-03-25 11:38:20 -0600

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,10 +6,8 @@ const stripe = Stripe('pk_test_TFvfAEmFeq8Dov8MNL136Nqy');
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`,
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
