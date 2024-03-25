2dae1857f92113167324e2b831bb96442c162f1d
Juan Villa <ing.villa.jimenez@gmail.com>
2024-03-25 10:43:10 -0600

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_TFvfAEmFeq8Dov8MNL136Nqy');
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
